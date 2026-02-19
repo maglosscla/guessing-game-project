5a20eb0ed143111c7c642749b9bc6dc4eca1a161
maglosscla <1234567+username@users.noreply.github.com>
2026-02-19 19:11:54 +0300

    added documention

--- game.py
+++ game.py
@@ -1,19 +1,19 @@
-import random
+import random #this brings the tool of rng into this code so this game can be made 
 
-print("🎮 Guess the Number Game")
-print("Computer picked a number 1-10. Guess it.")
+print("🎮 Guess the Number Game") #title of the game gets printed first 
+print("Computer picked a number 1-10. Guess it.") #instructions for how to play the game aka request
 
-secret = random.randint(1, 10,)
-attempts = 0
+secret = random.randint(1, 10,) # list of numbers you can pick from
+attempts = 0 
 
-while True:
+while True: 
     try:
-        guess = int(input("Your guess: "))
-        attempts += 1
+        guess = int(input("Your guess: ")) #user inputs a number from 1 to 10 
+        attempts += 1 # attemp tracker will be logged soon
         
-        if guess < secret:
+        if guess < secret: # if you guess 1 and the number is 5 then it print too low because of the fact you went under the value that's it
             print("Too low 📉")
-        elif guess > secret:
+        elif guess > secret: #if you guess 10 when the number is 1 you would have guessed too high and it would print a too high that's about it
             print("Too high 📈")
         else:
             print(f"✅ Correct! You got it in {attempts} tries")
